8a00b39b2a79d73ae025fcd19249bb07d6ff7ec1
Hironori Yamamoto <mr.nikoru918@gmail.com>
2022-08-12 15:42:15 +0900

    print namespaces/contexts when not tty

--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -19,44 +19,62 @@ pub mod update;
 
 pub enum SelectResult {
     Cancelled,
+    Listed,
     Selected(String),
 }
 
 pub fn select_context(skim_options: &SkimOptions, installed: &mut Installed) -> Result<SelectResult> {
     installed.contexts.sort_by(|a, b| a.item.name.cmp(&b.item.name));
     let mut context_names: Vec<_> = installed.contexts.iter().map(|c| c.item.name.clone()).collect();
-    context_names.reverse();
 
     if context_names.is_empty() {
         bail!("No contexts found");
     }
 
-    let item_reader = SkimItemReader::default();
-    let items = item_reader.of_bufread(Cursor::new(context_names.join("\n")));
-    let selected_items = Skim::run_with(skim_options, Some(items))
-        .map(|out| out.selected_items)
-        .unwrap_or_else(|| Vec::new());
-    if selected_items.is_empty() {
-        return Ok(SelectResult::Cancelled);
+    if atty::is(atty::Stream::Stdout) {
+        // NOTE: skim show the list of context names in reverse order
+        context_names.reverse();
+        let item_reader = SkimItemReader::default();
+        let items = item_reader.of_bufread(Cursor::new(context_names.join("\n")));
+        let selected_items = Skim::run_with(skim_options, Some(items))
+            .map(|out| out.selected_items)
+            .unwrap_or_else(|| Vec::new());
+        if selected_items.is_empty() {
+            return Ok(SelectResult::Cancelled);
+        }
+        Ok(SelectResult::Selected(selected_items[0].output().to_string()))
+    } else {
+        for c in context_names {
+            println!("{}", c);
+        }
+        Ok(SelectResult::Listed)
     }
-    Ok(SelectResult::Selected(selected_items[0].output().to_string()))
 }
 
 pub fn select_namespace(skim_options: &SkimOptions) -> Result<SelectResult> {
     let mut namespaces = kubectl::get_namespaces(None)?;
-    namespaces.reverse();
+    namespaces.sort();
 
     if namespaces.is_empty() {
         bail!("No namespaces found");
     }
 
-    let item_reader = SkimItemReader::default();
-    let items = item_reader.of_bufread(Cursor::new(namespaces.join("\n")));
-    let selected_items = Skim::run_with(skim_options, Some(items))
-        .map(|out| out.selected_items)
-        .unwrap_or_else(|| Vec::new());
-    if selected_items.is_empty() {
-        return Ok(SelectResult::Cancelled);
+    if atty::is(atty::Stream::Stdout) {
+        // NOTE: skim show the list of namespaces in reverse order
+        namespaces.reverse();
+        let item_reader = SkimItemReader::default();
+        let items = item_reader.of_bufread(Cursor::new(namespaces.join("\n")));
+        let selected_items = Skim::run_with(skim_options, Some(items))
+            .map(|out| out.selected_items)
+            .unwrap_or_else(|| Vec::new());
+        if selected_items.is_empty() {
+            return Ok(SelectResult::Cancelled);
+        }
+        Ok(SelectResult::Selected(selected_items[0].output().to_string()))
+    } else {
+        for n in namespaces {
+            println!("{}", n);
+        }
+        Ok(SelectResult::Listed)
     }
-    Ok(SelectResult::Selected(selected_items[0].output().to_string()))
 }
