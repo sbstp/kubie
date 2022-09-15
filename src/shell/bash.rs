26b63015be3109eca8e31d9e81e51d060b467c1c
Matt Campbell <mrcsys@rit.edu>
2022-09-15 12:38:34 -0400

    Add support for pre/post shell hooks to bash.

--- src/shell/bash.rs
+++ src/shell/bash.rs
@@ -66,6 +66,14 @@ unset KUBIE_PROMPT
         )?;
     }
 
+	if info.settings.hooks.start_ctx != "" {
+		write!(
+			temp_rc_file_buf,
+			"{}", 
+            info.settings.hooks.start_ctx
+		)?;
+	}
+
     temp_rc_file_buf.flush()?;
 
     let mut cmd = Command::new("bash");
@@ -75,6 +83,26 @@ unset KUBIE_PROMPT
 
     let mut child = cmd.spawn()?;
     child.wait()?;
+    
+	if info.settings.hooks.start_ctx != "" {
+		let temp_exit_hook_file = tempfile::Builder::new()
+			.prefix("kubie-bash-exit-hook")
+			.suffix(".bash")
+			.tempfile()?;
+		let mut temp_exit_hook_file_buf = BufWriter::new(temp_exit_hook_file.as_file());
+
+		write!(
+			temp_exit_hook_file_buf,
+			  "{}", info.settings.hooks.stop_ctx)?;
+
+		temp_exit_hook_file_buf.flush()?;
+		let mut exit_cmd = Command::new("bash");
+		exit_cmd.arg(temp_exit_hook_file.path());
+		info.env_vars.apply(&mut exit_cmd);
+
+        let mut child = exit_cmd.spawn()?;
+        child.wait()?;
+	}
 
     Ok(())
 }
